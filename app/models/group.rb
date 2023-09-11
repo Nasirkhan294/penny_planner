0aa15cb554157440a8496645f1d67d172d42e976
Nasir <nasirbhatti.nk@gmail.com>
2023-09-11 06:16:57 -0700

    Add authentication with devise gem

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,7 +1,4 @@
 class Group < ApplicationRecord
   belongs_to :user
-
   has_many :expenses
-
-  validates :name, presence: true
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ApplicationRecord
-  has_many :groups
-  has_many :expenses, through: groups
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable, :confirmable
 
-  validates :name, presence: true
+  has_many :groups
+  has_many :expenses
 end
