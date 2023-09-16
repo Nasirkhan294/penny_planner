c5d758c6b045ff295d80d617ba7dceec87c2400b
Nasir <nasirbhatti.nk@gmail.com>
2023-09-15 22:44:43 -0700

    Update puma file

--- db/schema.rb
+++ db/schema.rb
@@ -10,59 +10,59 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_09_14_090350) do
+ActiveRecord::Schema[7.0].define(version: 20_230_914_090_350) do
   # These are extensions that must be enabled in order to support this database
-  enable_extension "plpgsql"
+  enable_extension 'plpgsql'
 
-  create_table "bills", force: :cascade do |t|
-    t.string "name"
-    t.string "amount"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.bigint "author_id", null: false
-    t.bigint "group_id", null: false
-    t.string "description"
-    t.index ["author_id"], name: "index_bills_on_author_id"
-    t.index ["group_id"], name: "index_bills_on_group_id"
+  create_table 'bills', force: :cascade do |t|
+    t.string 'name'
+    t.string 'amount'
+    t.datetime 'created_at', null: false
+    t.datetime 'updated_at', null: false
+    t.bigint 'author_id', null: false
+    t.bigint 'group_id', null: false
+    t.string 'description'
+    t.index ['author_id'], name: 'index_bills_on_author_id'
+    t.index ['group_id'], name: 'index_bills_on_group_id'
   end
 
-  create_table "group_bills", force: :cascade do |t|
-    t.bigint "group_id", null: false
-    t.bigint "bill_id", null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["bill_id"], name: "index_group_bills_on_bill_id"
-    t.index ["group_id"], name: "index_group_bills_on_group_id"
+  create_table 'group_bills', force: :cascade do |t|
+    t.bigint 'group_id', null: false
+    t.bigint 'bill_id', null: false
+    t.datetime 'created_at', null: false
+    t.datetime 'updated_at', null: false
+    t.index ['bill_id'], name: 'index_group_bills_on_bill_id'
+    t.index ['group_id'], name: 'index_group_bills_on_group_id'
   end
 
-  create_table "groups", force: :cascade do |t|
-    t.string "name"
-    t.string "icon"
-    t.string "description"
-    t.bigint "user_id", null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["user_id"], name: "index_groups_on_user_id"
+  create_table 'groups', force: :cascade do |t|
+    t.string 'name'
+    t.string 'icon'
+    t.string 'description'
+    t.bigint 'user_id', null: false
+    t.datetime 'created_at', null: false
+    t.datetime 'updated_at', null: false
+    t.index ['user_id'], name: 'index_groups_on_user_id'
   end
 
-  create_table "users", force: :cascade do |t|
-    t.string "name"
-    t.string "bio"
-    t.string "profile_picture"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.string "email", default: "", null: false
-    t.string "encrypted_password", default: "", null: false
-    t.string "reset_password_token"
-    t.datetime "reset_password_sent_at"
-    t.datetime "remember_created_at"
-    t.index ["email"], name: "index_users_on_email", unique: true
-    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+  create_table 'users', force: :cascade do |t|
+    t.string 'name'
+    t.string 'bio'
+    t.string 'profile_picture'
+    t.datetime 'created_at', null: false
+    t.datetime 'updated_at', null: false
+    t.string 'email', default: '', null: false
+    t.string 'encrypted_password', default: '', null: false
+    t.string 'reset_password_token'
+    t.datetime 'reset_password_sent_at'
+    t.datetime 'remember_created_at'
+    t.index ['email'], name: 'index_users_on_email', unique: true
+    t.index ['reset_password_token'], name: 'index_users_on_reset_password_token', unique: true
   end
 
-  add_foreign_key "bills", "groups"
-  add_foreign_key "bills", "users", column: "author_id"
-  add_foreign_key "group_bills", "bills"
-  add_foreign_key "group_bills", "groups"
-  add_foreign_key "groups", "users"
+  add_foreign_key 'bills', 'groups'
+  add_foreign_key 'bills', 'users', column: 'author_id'
+  add_foreign_key 'group_bills', 'bills'
+  add_foreign_key 'group_bills', 'groups'
+  add_foreign_key 'groups', 'users'
 end
